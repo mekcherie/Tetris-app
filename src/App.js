d6f48fee1c82e7fc1be8459e2b7ffe6d56a26339
Meklit Cherie <makywonde347@yahoo.com>
2021-06-29 18:18:19 -0700

    Added initial message popup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NextBlock from './components/NextBlock';
 import GridSquare from './components/GridSquare';
 import ScoreBoard from './components/ScoreBoard';
 import Controls from './components/Controls';
+import MessagePopup from './components/MessagePopup';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <NextBlock />
       <ScoreBoard />
       <Controls />
+      <MessagePopup />
     </div>
   );
 }
