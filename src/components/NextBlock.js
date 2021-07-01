f5b9c3ea7bd7ad0c492a92e8e123ef417aada1d2
Meklit Cherie <makywonde347@yahoo.com>
2021-07-01 16:27:49 -0700

    Added connection for next block

--- src/components/NextBlock.js
+++ src/components/NextBlock.js
@@ -1,20 +1,21 @@
+import { useSelector } from 'react-redux'
+import { shapes } from '../shapes'
 import React from 'react'
 import GridSquare from './GridSquare'
 
-// Draws the "next" block view showing the next block to drop
 export default function NextBlock(props) {
-
-    const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-    // Map the block to the grid
+    const nextShape = useSelector((state) => state.game.nextShape)
+    const box = shapes[nextShape][0]
+    // const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
     const grid = box.map((rowArray, row) => {
         return rowArray.map((square, col) => {
             return <GridSquare key={`${row}${col}`} color={square} />
         })
     })
-
     return (
         <div className="next-block">
             {grid}
         </div>
     )
 }
+
--- src/utils/index.js
+++ src/utils/index.js
@@ -120,24 +120,15 @@ export const gridDefault = () => {
   }
   export const defaultState = () => {
     return {
-      // Create an empty grid
       grid: gridDefault(),
-      // Get a new random shape
       shape: randomShape(),
-      // set rotation of the shape to 0
       rotation: 0,
-      // set the 'x' position of the shape to 5 and y to -4, which puts the shape in the center of the grid, above the top
       x: 5,
       y: -4,
-      // set the index of the next shape to a new random shape
       nextShape: randomShape(),
-      // Tell the game that it's currently running
       isRunning: true,
-      // Set the score to 0
       score: 0,
-      // Set the default speed
       speed: 1000,
-      // Game isn't over yet
       gameOver: false
     }
   }
