6baa77f6eec067725c4a0c7e29aa4f3d2d145e31
Meklit Cherie <makywonde347@yahoo.com>
2021-07-01 18:26:25 -0700

    rotate reducer

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -1,46 +1,55 @@
 import {
-    MOVE_RIGHT, MOVE_LEFT, MOVE_DOWN, ROTATE,
-    PAUSE, RESUME, RESTART, GAME_OVER
-  } from '../actions'
-  import { defaultState } from '../utils'
+  defaultState,
+  nextRotation,
+  canMoveTo } from '../utils'
   
   const gameReducer = (state = defaultState(), action) => {
+    const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state
+
     switch(action.type) {
       case ROTATE:
-  
+        const newRotation = nextRotation(shape, rotation)
         return state
+     case MOVE_RIGHT:
+    if (canMoveTo(shape, grid, x + 1, y, rotation)) {
+      return { ...state, x: x + 1 }
+    }
+    return state
   
-      case MOVE_RIGHT:
-  
-        return state
+  //       return state
   
       case MOVE_LEFT:
-  
-        return state
-  
-      case MOVE_DOWN:
-  
-        return state
-  
-      case RESUME:
-  
-        return state
-  
-      case PAUSE:
-  
-        return state
-  
-      case GAME_OVER:
-  
-        return state
-  
-      case RESTART:
-  
-        return state
-  
-      default:
-        return state
-    }
+    // subtract 1 from the x and check if this new position is possible by calling `canMoveTo()
+    if (canMoveTo(shape, grid, x - 1, y, rotation)) {
+      return { ...state, x: x - 1 }
   }
+  return state
   
-  export default gameReducer
+  //       return state
+  
+  //     case MOVE_DOWN:
+  
+  //       return state
+  
+  //     case RESUME:
+  
+  //       return state
+  
+  //     case PAUSE:
+  
+  //       return state
+  
+  //     case GAME_OVER:
+  
+  //       return state
+  
+  //     case RESTART:
+  
+  //       return state
+  
+  //     default:
+  //       return state
+  //   }
+  // }
+  
+  // export default gameReducer
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,134 +1,36 @@
-export const random = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-  }
-  // Returns the default grid
-export const gridDefault = () => {
-    const rows = 18
-    const cols = 10
-    const array = []
-    for (let row = 0; row < rows; row++) {
-        array.push([])
-        for (let col = 0; col < cols; col++) {
-          array[row].push(0)
-        }
-    }
-    return array
-  }
-  export const shapes = [
-    [[[0,0,0,0],
-      [0,0,0,0],
-      [0,0,0,0],
-      [0,0,0,0]]],
-  
-    [[[0,0,0,0],
-      [1,1,1,1],
-      [0,0,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,0,0],
-      [0,1,0,0],
-      [0,1,0,0]]],
-  
-    [[[0,0,0,0],
-      [1,1,1,0],
-      [0,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [1,1,0,0],
-      [0,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [1,1,1,0],
-      [0,0,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,1,0],
-      [0,1,0,0],
-      [0,0,0,0]]],
-  
-    [[[0,0,0,0],
-      [1,1,1,0],
-      [1,0,0,0],
-      [0,0,0,0]],
-  
-     [[1,1,0,0],
-      [0,1,0,0],
-      [0,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,0,1,0],
-      [1,1,1,0],
-      [0,0,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,0,0],
-      [0,1,1,0],
-      [0,0,0,0]]],
-  
-    [[[1,0,0,0],
-      [1,1,1,0],
-      [0,0,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,1,0],
-      [0,1,0,0],
-      [0,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,0,0,0],
-      [1,1,1,0],
-      [0,0,1,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,0,0],
-      [1,1,0,0],
-      [0,0,0,0]]],
-  
-    [[[0,0,0,0],
-      [1,1,0,0],
-      [0,1,1,0],
-      [0,0,0,0]],
-  
-     [[0,0,1,0],
-      [0,1,1,0],
-      [0,1,0,0],
-      [0,0,0,0]]],
-  
-    [[[0,0,0,0],
-      [0,1,1,0],
-      [1,1,0,0],
-      [0,0,0,0]],
-  
-     [[0,1,0,0],
-      [0,1,1,0],
-      [0,0,1,0],
-      [0,0,0,0]]],
+import { shapes } from '../utils'
+export const nextRotation = (shape, rotation) => {
+  return (rotation + 1) % shapes[shape].length
+}
 
-    [[[0,1,1,0],
-      [0,1,1,0],
-      [0,0,0,0],
-      [0,0,0,0]]]
-  ]
-  export const randomShape = () => {
-    return random(1, shapes.length - 1)
+export const canMoveTo = (shape, grid, x, y, rotation) => {
+  const currentShape = shapes[shape][rotation]
+  // Loop through all rows and cols of the **shape**
+  for (let row = 0; row < currentShape.length; row++) {
+      for (let col = 0; col < currentShape[row].length; col++) {
+          // Look for a 1 here
+          if (currentShape[row][col] !== 0) {
+              // x offset on grid
+              const proposedX = col + x
+              // y offset on grid
+              const proposedY = row + y
+              if (proposedY < 0) {
+                  continue
+              }
+              // Get the row on the grid
+              const possibleRow = grid[proposedY]
+              // Check row exists
+              if (possibleRow) {
+                  // Check if this column in the row is undefined, if it's off the edges, 0, and empty
+                  if (possibleRow[proposedX] === undefined || possibleRow[proposedX] !== 0) {
+                      // undefined or not 0 and it's occupied we can't move here.
+                      return false
+                  }
+              } else {
+                  return false
+              }
+          }
+      }
   }
-  export const defaultState = () => {
-    return {
-      grid: gridDefault(),
-      shape: randomShape(),
-      rotation: 0,
-      x: 5,
-      y: -4,
-      nextShape: randomShape(),
-      isRunning: true,
-      score: 0,
-      speed: 1000,
-      gameOver: false
-    }
-  }
+  return true
+}
